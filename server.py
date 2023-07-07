b668ad5cde91c64a8652f603f5be97e37fdb4c49
Sidrotul Munawaroh <94377420+SidrotulMunawaroh@users.noreply.github.com>
2023-07-07 11:40:27 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,90 +1,116 @@
 import socket
 import threading
+import pickle
+import random
 
-HOST = '127.0.0.2'
-PORT = 8002
+# Inisialisasi server socket
+host = "localhost"
+port = 12239
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.bind((host, port))
+server_socket.listen()
+print("Server is running.")
 
+# Daftar room
 rooms = {}
-player_marks = {}
-room_locks = {}
 
+player1 = "X"
+player2 = "O"
+# Kelas untuk mengelola setiap room
+class Room:
+    def __init__(self, room_id):
+        self.room_id = room_id
+        self.players = []
+        self.current_turn = 0
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
 
-def handle_client(client_socket, room_id, player_id):
+    def add_player(self, conn):
+        self.players.append(conn)
+
+    def broadcast(self, data):
+        serialized_data = pickle.dumps(data)
+        for player in self.players:
+            player.send(serialized_data)
+
+    def handle_turn(self, conn, row, col):
+        if conn == self.players[self.current_turn]:
+            symbol = player1 if self.current_turn == 0 else player2
+            if self.board[row][col] == " ":
+                self.board[row][col] = symbol
+                self.broadcast(("update", self.board))
+                winner = self.check_winner()
+                if winner:
+                    self.broadcast(("winner", winner))
+                    self.reset()
+                elif self.check_tie():
+                    self.broadcast(("tie", None))
+                    self.reset()
+                else:
+                    self.current_turn = (self.current_turn + 1) % 2
+        
+    
+    def check_winner(self):
+        for i in range(3):
+            if self.board[i][0] == self.board[i][1] == self.board[i][2] != " ":
+                return self.board[i][0]
+            if self.board[0][i] == self.board[1][i] == self.board[2][i] != " ":
+                return self.board[0][i]
+            if self.board[0][0] == self.board[1][1] == self.board[2][2] != " ":
+                return self.board[0][0]
+            if self.board[0][2] == self.board[1][1] == self.board[2][0] != " ":
+                return self.board[0][2]
+            return None
+     
+
+    def check_tie(self):
+        for row in self.board:
+            if " " in row:
+                return False
+        return True
+
+    def reset(self):
+        self.board = [[" " for _ in range(3)] for _ in range(3)]
+        self.current_turn = 0
+
+    def toss_coin(self):
+        return random.choice([0, 1])
+
+# Fungsi untuk mengelola koneksi client dalam thread terpisah
+def handle_client(conn):
+    room_id = conn.recv(1024).decode()
+    if room_id not in rooms:
+        rooms[room_id] = Room(room_id)
+    room = rooms[room_id]
+    room.add_player(conn)
+    conn.send(pickle.dumps("Waiting for opponent..."))
+    if len(room.players) == 2:
+        toss_result = room.toss_coin()
+        if toss_result == 0:
+            print("Player 1: You got X and the first turn")
+        elif toss_result == 1:
+            print("Player 2: You got O and the second turn")
+        
+        room.broadcast(("toss_result", toss_result))
+        room.current_turn = toss_result
+        room.broadcast(("start", None))
     while True:
         try:
-            data = client_socket.recv(1024).decode()
-            if not data:
-                break
-            if data == 'quit':
-                break
-            if data.isdigit():
-                opponent_id = 1 - player_id
-                opponent_socket = rooms[room_id][opponent_id]
-                opponent_socket.sendall(str.encode(data))
+            data = conn.recv(1024)
+            if data:
+                row, col = pickle.loads(data)
+                room.handle_turn(conn, row, col)
             else:
-                mark, pos = data[0], int(data[1:])
-                room_lock = room_locks[room_id]
-                with room_lock:
-                    board = rooms[room_id][2]
-                    if mark == player_marks[player_id] and board[pos] == '-' and (
-                            (player_marks[player_id] == 'X' and not rooms[room_id][3]) or
-                            (player_marks[player_id] == 'O' and rooms[room_id][3])
-                    ):
-                        board[pos] = mark
-                        if player_marks[player_id] == 'X':
-                            rooms[room_id][3] = True
-                        opponent_id = 1 - player_id
-                        opponent_socket = rooms[room_id][opponent_id]
-                        opponent_socket.sendall(str.encode(data))
-                        opponent_socket.sendall(str.encode(opponent_socket.recv(1024).decode()))  # Send the updated board state to the opponent
-                    else:
-                        if mark == player_marks[player_id]:
-                            client_socket.sendall(str.encode('-2'))  # Invalid move by the player
-                        else:
-                            client_socket.sendall(str.encode('-3'))  # Invalid move, it's not player's turn
+                raise Exception("Disconnected")
         except:
+            room.players.remove(conn)
+            conn.close()
             break
 
-    client_socket.close()
-    if room_id in rooms:
-        del rooms[room_id]
-
-
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(2)
-    print("Server is running.")
-
+# Fungsi untuk menerima koneksi dan memulai thread baru
+def accept_connections():
     while True:
-        client_socket, addr = server_socket.accept()
-        room_id = client_socket.recv(1024).decode()
-        if room_id in rooms:
-            if len(rooms[room_id]) < 4:
-                player_id = len(rooms[room_id])
-                rooms[room_id].append(client_socket)
-                player_marks[player_id] = 'O' if player_id % 2 == 1 else 'X'
-                client_socket.sendall(str.encode(str(player_id)))
-                print(f"Player {player_id} joined room {room_id}")
-                if len(rooms[room_id]) == 4:
-                    room_locks[room_id] = threading.Lock()
-                    thread = threading.Thread(target=handle_client, args=(rooms[room_id][1], room_id, 1))
-                    thread.start()
-                    thread = threading.Thread(target=handle_client, args=(rooms[room_id][2], room_id, 2))
-                    thread.start()
-                    rooms[room_id][3] = False  # Flag to track whether player X has made a move
-            else:
-                client_socket.sendall(str.encode('-1'))  # Room is full
-                client_socket.close()
-        else:
-            rooms[room_id] = [client_socket]
-            player_id = 0
-            player_marks[player_id] = 'X'
-            client_socket.sendall(str.encode(str(player_id)))
-            print(f"Player {player_id} created room {room_id}")
+        conn, addr = server_socket.accept()
+        threading.Thread(target=handle_client, args=(conn,)).start()
 
-    server_socket.close()
-
-
-start_server()
+# Memulai server
+accept_connections()
